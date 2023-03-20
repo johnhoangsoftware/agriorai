dd45b742a318f481911a17d526c2c362f1a7295d
johnhoangsoftware <johnhoangsoftware@gmail.com>
2023-03-21 03:53:01 +0700

    feat: get balance wallet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
-import React, { useEffect } from 'react';
-import suggestWallet from './components/suggestWallet';
-import { CosmosBalanceCard } from './components/Navbar';
+import React from 'react';
+import CosmosBalanceForm from './components/CosmosBalanceForm';
+
 function App() {
-
-  useEffect(() => {
-    suggestWallet();
-  });
-
   return (
     <div className="App">
-      <CosmosBalanceCard></CosmosBalanceCard>
+      <CosmosBalanceForm></CosmosBalanceForm>
     </div>
   );
 }
