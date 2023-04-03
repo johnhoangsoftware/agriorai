74a4db807fb93dac43c0301fa7d090f354f3feb8
Nguyen Duc Hoang <nguyenduchoang@hoangks5.local>
2023-04-03 18:18:31 +0700

    create location map

--- src/mapLocation.py
+++ src/mapLocation.py
@@ -12,14 +12,7 @@ key = os.getenv("MAPBOX_ACCESS_TOKEN")
 lat = 21.0277644
 long = 105.8341598
 
-# Địa chỉ lat và long của địa điểm tạo marker trên map (địa điểm đó sẽ được đánh dấu)
-lat_marker = 21.0273644
-long_marker = 105.8351598
+# tạo link nhúng map game với lat và long của địa điểm location với zoom 15
+url = f"https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s+000({long},{lat})/{long},{lat},15,0,0/600x400?access_token={key}"
 
-# Thông tin của marker trên map như tên, màu sắc, kích thước ...
-marker = f"pin-s+000({long_marker},{lat_marker})"
-# Tạo link nhúng bản đồ với địa điểm location và marker
-link = f"https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s+000({long_marker},{lat_marker})/{long},{lat},15,0,0/600x400?access_token={key}"
-
-# In ra link nhúng bản đồ
-print(link)
+print(url)
