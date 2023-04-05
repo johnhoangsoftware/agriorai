85868b3e7705516c34b4225b2d21819f92bee6e2
Nguyen Duc Hoang <nguyenduchoang@hoangks5.local>
2023-04-05 15:12:35 +0700

    update

--- main.py
+++ main.py
@@ -171,8 +171,10 @@ async def predict_disease_apple_with_image_of_apple_leaf(file: UploadFile = File
         output = model(image)
         _, preds = torch.max(output, 1)
         class_name = classes[preds]
-        
-        return {"class_name": class_name, "accuracy": float(torch.max(F.softmax(output, dim=1)))}
+        if class_name.split('___')[0] != 'Apple':
+            return {"class_name": None}
+        else:
+            return {"class_name": class_name, "accuracy": float(torch.max(F.softmax(output, dim=1)))}
     
 @app.post("/cherry/v1",tags=["Cherry"],description="Predict Disease Cherry With Image Of Cherry Leaf (Powdery Mildew, Healthy)")
 async def predict_disease_cherry_with_image_of_cherry_leaf(file: UploadFile = File(...)):
@@ -366,7 +368,6 @@ async def predict_disease_squash_with_image_of_squash_leaf(file: UploadFile = Fi
         else:
             return {"class_name": class_name, "accuracy": float(torch.max(F.softmax(output, dim=1)))}
         
-
 
 
 """ # requirements.txt
