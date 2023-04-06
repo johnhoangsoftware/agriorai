f0f03c811786fc0fbbe6aae70d0cd3b46e6c6771
hoangks5 <81577195+hoangks5@users.noreply.github.com>
2023-04-06 16:43:00 +0700

    change port 8000

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template("index.html") 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
