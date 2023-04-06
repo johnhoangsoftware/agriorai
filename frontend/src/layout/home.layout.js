903a33e67df373d5c4bb7d295043f2eb50e4a096
johnhoangsoftware <johnhoangsoftware@gmail.com>
2023-04-07 03:08:12 +0700

    feat: new fe

--- frontend/src/layout/home.layout.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Helmet } from "react-helmet-async";
-const HomeLayout = ({ children }) => {
-  return (
-    <>
-    
-    </>
-  );
-}
---
+++ src/config/routes.ts
@@ -0,0 +1,25 @@
+const routes = {
+  home: '/',
+  minimal: '/minimal',
+  livePricing: '/live-pricing',
+  retro: '/retro',
+  swap: '/swap',
+  liquidity: '/liquidity',
+  liquidityPosition: '/liquidity-position',
+  farms: '/farms',
+  createNft: '/create-nft',
+  nftDetails: '/nft-details',
+  search: '/search',
+  notification: '/notifications',
+  vote: '/vote',
+  proposals: '/proposals',
+  createProposal: '/proposals/create',
+  charts: '/charts',
+  profile: '/profile',
+  portfolio: '/profile?view=portfolio',
+  history: '/profile?view=history',
+  classic: '/classic',
+  coinDetails: '/coin-details',
+};
+
+export default routes;
